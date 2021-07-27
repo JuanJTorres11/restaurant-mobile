5cbb905b72155a454bf60c03456af3794678099e
Juan José Torres <juanjtorres@pm.me>
2021-07-27 15:26:06 -0500

    load data finished

--- lib/screens/LoadData.dart
+++ lib/screens/LoadData.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:restaurant_mobile/utils/network.dart';
 
 class LoadData extends StatefulWidget {
   @override
@@ -6,55 +8,104 @@ class LoadData extends StatefulWidget {
 }
 
 class _LoadDataState extends State<LoadData> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _dateController = TextEditingController();
+  var _selectedDate = DateTime.now();
+  var _result;
+  var _sended = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Load Data"),
+      ),
       body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Card(
-                child: Form(
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Container(
-                          child: const Text(
-                            'Ingresar fecha',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    child: const Text(
+                      'Select a date to load the data',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    alignment: Alignment.center,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Current date: ",
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                        TextFormField(
-                          controller: _dateController,
-                          decoration: const InputDecoration(labelText: 'fecha'),
-                          validator: (String? value) {
-                            if (value!.isEmpty) {
-                              return 'Ingresa la fecha';
-                            }
-                            return null;
-                          },
-                          keyboardType: TextInputType.datetime,
+                        Text("${_selectedDate.toLocal()}".split(' ')[0]),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 30),
+                          child: TextButton(
+                            onPressed: () => _selectDate(context),
+                            child: Text('Select date'),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(30.0),
+                              ),
+                              padding: EdgeInsets.all(10)),
+                          onPressed: () {
+                            loadData(_selectedDate).then((value) {
+                              setState(() {
+                                _result = value;
+                                _sended = false;
+                              });
+                            });
+                            setState(() {
+                              _sended = true;
+                            });
+                          },
+                          child: Text("Load Data")),
+                      _sended == true
+                          ? CircularProgressIndicator()
+                          : _result == null
+                              ? Container()
+                              : _result
+                                  ? Icon(Icons.check_circle_outline,
+                                      size: 50, color: Colors.green)
+                                  : Icon(Icons.error_outline,
+                                      size: 50, color: Colors.red)
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
+      ),
     );
   }
 
-  /// Clean up the controller when the Widget is disposed
-  @override
-  void dispose() {
-    _dateController.dispose();
-    super.dispose();
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialEntryMode: DatePickerEntryMode.input,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2021, 1, 1),
+        lastDate: DateTime.now());
+    if (picked != null)
+      setState(() {
+        _selectedDate = picked;
+      });
   }
 }
--- lib/utils/network.dart
+++ lib/utils/network.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<bool> verifyHost(String url) async {
   String? res;
@@ -9,4 +10,19 @@ Future<bool> verifyHost(String url) async {
     print(e);
   }
   return res == "hello world";
+}
+
+Future<bool> loadData(DateTime date) async {
+  String? res;
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var host = prefs.getString("host");
+  try {
+    var r = await http.get(Uri.parse("http://$host/load/${date.millisecondsSinceEpoch~/1000}"));
+    print("http://$host/${date.millisecondsSinceEpoch~/1000}");
+    res = r.body;
+    print(res);
+  } on Exception catch (e) {
+    print(e);
+  }
+  return res!.contains("Success");
 }
