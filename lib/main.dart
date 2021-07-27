9b202379205e9d4b138089d2c06b007876d4d48f
Juan José Torres <juanjtorres@pm.me>
2021-07-26 23:45:23 -0500

    connection to host

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,105 +11,113 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Restaurante',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var _host = false;
+  var _showMessage = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _hostController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Bienvenido"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          child: Form(
+              key: _formKey,
+              child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 30.0, right: 30.0, top: 5.0, bottom: 5.0),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        TextFormField(
+                          controller: _hostController,
+                          decoration: const InputDecoration(
+                              labelText: 'Ingresa la url y puerto de la API',
+                              prefixText: "http://",
+                              focusColor: Colors.grey),
+                          validator: (String? value) {
+                            if (value!.isEmpty)
+                              return 'Ingresa la url y puerto';
+                            return null;
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: ConstrainedBox(
+                            constraints:
+                                BoxConstraints.tightFor(width: double.infinity),
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    primary: Theme.of(context)
+                                        .primaryColor, // background
+                                    onPrimary: Colors.black, // foreground
+                                    shape: new RoundedRectangleBorder(
+                                      borderRadius:
+                                          new BorderRadius.circular(30.0),
+                                    ),
+                                    padding: EdgeInsets.all(16)),
+                                onPressed: () async {
+                                  if (_formKey.currentState!.validate()) {
+                                    var con = await verifyHost(_hostController.text);
+                                    if (con){
+                                      setState(() {
+                                        _showMessage = false;
+                                      });
+                                      SharedPreferences prefs = await SharedPreferences.getInstance();
+                                      prefs.setString("host", _hostController.text);
+
+                                    }
+                                    else {
+                                      setState(() {
+                                        _showMessage = true;
+                                      });
+                                    }
+                                  }
+                                },
+                                child: Text("Enviar")),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 10),
+                          alignment: Alignment.center,
+                          child: Text(
+                            _showMessage == false
+                                ? ''
+                                : "La dirección indicada es inválida",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.redAccent),
+                          ),
+                        ),
+                      ])))),
     );
+  }
+
+  Future<bool> verifyHost(String url) async {
+    String? res;
+    try {
+      var r = await http.get(Uri.parse("http://" + url));
+      res = r.body;
+    } on Exception catch (e) {
+      print(e);
+    }
+    return res == "hello world";
   }
 }
